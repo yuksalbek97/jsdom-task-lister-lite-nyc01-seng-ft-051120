9d6ae089e40e0f56d5d104273e24d322998ec990
yuksalbek97 <yuksal97@gmail.com>
2020-07-14 17:24:06 -0400

    Done.

--- src/index.js
+++ src/index.js
@@ -3,22 +3,52 @@
 // As a user, the task string that I provided should appear on the DOM after the submit button has been activated.
 
 
-
-const newTasksUl = document.getElementById("tasks")
-
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  // your code here 
+  // const taskList = new taskList()
+  const newTaskForm = document.getElementById('create-task-form')
+  const newTaskDescription = document.getElementById('new-task-description')
+  const taskContainer = document.getElementById('tasks')
 
-  const newTaskForm = document.getElementById("create-task-form")
-  const newTaskDescription = document.getElementById("new-task-description")
+
+newTaskForm.addEventListener('submit', newTask);
 
 
-  newTaskForm.addEventListener("submit", createTask)
-});
 
-const createTask = e => {
-const newTask = document.createElement("li")
-const newTaskDescription = document.getElementById("new-task-description")
-newTask.innerText = newTaskDescription.value 
-newTasksUl.append(newTask)
+ 
+
+
+function newTask(event){
+  event.preventDefault();
+
+  const newLi = document.createElement('li')
+  newLi.className = 'lists'
+  const btn = document.createElement('button')
+  btn.id = 'buttonRemove'
+  newLi.className = 'newItems'
+  newLi.innerText = newTaskDescription.value
+  taskContainer.append(newLi)
+  taskContainer.append(btn)
+  event.target.reset()
+
+   
+
 }
+
+
+
+
+
+
+
+
+})
+
+
+// function resetList(){
+//    document.getElementById('new-task-description').reset()
+// }
+
+
+// a delete function that will remove tasks from your list
+// 
