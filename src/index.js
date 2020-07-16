b96787b142dff798813e9e756fc7c0d4f18eea22
yuksalbek97 <yuksal97@gmail.com>
2020-07-16 10:31:19 -0400

    Done.

--- src/index.js
+++ src/index.js
@@ -2,47 +2,38 @@
 // As a user, I should be able to click some form of a submit button.
 // As a user, the task string that I provided should appear on the DOM after the submit button has been activated.
 
-
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here 
-  // const taskList = new taskList()
-  const newTaskForm = document.getElementById('create-task-form')
-  const newTaskDescription = document.getElementById('new-task-description')
-  const taskContainer = document.getElementById('tasks')
-
-
-newTaskForm.addEventListener('submit', newTask);
 
 
 
- 
+const form = document.querySelector("form")
 
+form.addEventListener("submit", function(e){
+  e.preventDefault()
 
-function newTask(event){
-  event.preventDefault();
+  const task = e.target['new-task-description'].value
+  const taskLi = document.createElement('li')
+  taskLi.textContent = task
 
-  const newLi = document.createElement('li')
-  newLi.className = 'lists'
-  const btn = document.createElement('button')
-  btn.id = 'buttonRemove'
-  newLi.className = 'newItems'
-  newLi.innerText = newTaskDescription.value
-  taskContainer.append(newLi)
-  taskContainer.append(btn)
-  event.target.reset()
+  const button = document.createElement("button")
+  button.textContent = "X"
+  taskLi.append(button)
 
-   
+  button.addEventListener("click", function(e){
+    const taskLi = e.target.parentElement
+    taskLi.remove()
+  })
 
-}
-
-
-
-
-
-
-
-
+  const tasks = document.getElementById('tasks')
+  tasks.append(taskLi)
 })
+});
+
+
+
+
+
+
 
 
 // function resetList(){
